36565599b3976fce92cbfc59485e601b336cd404
GustavoM137 <gustauvomenezes@gmail.com>
2026-09-22 18:06:43 -0300

    add files

--- q3_IA.c
+++ q3_IA.c
@@ -2,28 +2,42 @@
 
 int main(void) {
     int n;
-    scanf("%d", &n);
+
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Entrada invalida\n");
+        return 1;
+    }
 
     int pixel, xmin, xmax;
 
-    scanf("%d", &pixel);
+    if (scanf("%d", &pixel) != 1 || pixel < 0 || pixel > 255) {
+        printf("Entrada invalida\n");
+        return 1;
+    }
     xmin = xmax = pixel;
 
-    for (int i = 2; i <= n; i++) {
-        scanf("%d", &pixel);
-        if (pixel < xmin)
-            xmin = pixel;
-        if (pixel > xmax)
-            xmax = pixel;
+    for (int i = 1; i < n; i++) {
+        if (scanf("%d", &pixel) != 1 || pixel < 0 || pixel > 255) {
+            printf("Entrada invalida\n");
+            return 1;
+        }
+        if (pixel < xmin) xmin = pixel;
+        if (pixel > xmax) xmax = pixel;
     }
 
     int x;
-    scanf("%d", &x);
+    if (scanf("%d", &x) != 1 || x < 0 || x > 255) {
+        printf("Entrada invalida\n");
+        return 1;
+    }
 
-    double xLinha = (double)(x - xmin) / (xmax - xmin);
+    if (xmax == xmin) {
+        printf("Nao e possivel normalizar (xmax = xmin)\n");
+        return 1;
+    }
 
-    printf("%d %d\n", xmin, xmax);
-    printf("%.2f\n", xLinha);
+    float xLinha = (float)(x - xmin) / (xmax - xmin);
+    printf("%.4f\n", xLinha);
 
     return 0;
 }
