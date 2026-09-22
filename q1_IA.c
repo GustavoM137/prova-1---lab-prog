cc8db5f6c61b226e13a21d27c357a70d59a76e74
GustavoM137 <gustauvomenezes@gmail.com>
2026-09-22 18:00:55 -0300

    add files
    Added input validation for RGB values and M.

--- q1_IA.c
+++ q1_IA.c
@@ -2,11 +2,20 @@
 
 int main(void) {
     int R, G, B, M;
-    scanf("%d %d %d %d", &R, &G, &B, &M);
 
-    int bit2 = (M >> 2) & 1;
+    if (scanf("%d %d %d %d", &R, &G, &B, &M) != 4) {
+        printf("Entrada invalida\n");
+        return 1;
+    }
+
+    if (R < 0 || R > 255 || G < 0 || G > 255 || B < 0 || B > 255 || M < 0 || M > 7) {
+        printf("Entrada invalida\n");
+        return 1;
+    }
+
+    int bit2 = (M >> 2) & 1; /* bit mais significativo de M */
     int bit1 = (M >> 1) & 1;
-    int bit0 = M & 1;
+    int bit0 = M & 1;        /* bit menos significativo de M */
 
     R = (R & ~1) | bit2;
     G = (G & ~1) | bit1;
